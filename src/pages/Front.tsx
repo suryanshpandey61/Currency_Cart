1e4e4d3999ab168aa04084a07805f869615e00e1
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-16 19:46:44 +0530

    Front page responsiveness

--- src/pages/Front.tsx
+++ src/pages/Front.tsx
@@ -11,10 +11,10 @@ const Front: React.FC = () => {
   };
 
   return (
-    <div className="w-[100%] pt-[5%]  min-h-screen front-bg">
+    <div className="w-[100%] lg:pt-[5%] pt-[50px]  min-h-screen front-bg">
       <div className="lg:w-[70%] w-[90%] border rounded-md  p-[2%] mx-auto  flex flex-col">
         {/* Amount div  */}
-        <div className="flex lg:w-[80%] lg:pt-0 pt-2 w-[90%] justify-between mx-auto">
+        <div className="flex lg:w-[80%] lg:pt-0 pt-5 w-[90%] justify-between mx-auto">
           <h1 className="lg:text-[2rem] text-[1.5rem]">Amount</h1>
           <input
             type="number"
@@ -25,7 +25,7 @@ const Front: React.FC = () => {
           />
         </div>
         {/* From div  */}
-        <div className="flex mx-auto justify-between mt-[2%] lg:w-[80%] lg:pt-0 pt-2 w-[90%]">
+        <div className="flex mx-auto justify-between mt-[2%] lg:w-[80%] lg:pt-0 pt-5 w-[90%]">
           <h1 className="lg:text-[2rem] text-[1.5rem] ">From</h1>
           <input
             type="text"
@@ -35,7 +35,7 @@ const Front: React.FC = () => {
           />
         </div>
         {/* To div  */}
-        <div className="flex mx-auto justify-between mt-[2%] lg:w-[80%] lg:pt-0 pt-2  w-[90%]">
+        <div className="flex mx-auto justify-between mt-[2%] lg:w-[80%] lg:pt-0 pt-5  w-[90%]">
           <h1 className="lg:text-[2rem] text-[1.5rem]">To</h1>
           <input
             type="text"
@@ -44,14 +44,14 @@ const Front: React.FC = () => {
             className="border lg:w-[80%] w-[60%] pl-[2%] lg:text-[1.5rem] text-[1.2rem] rounded-md"
           />
         </div>
-        <div className="flex mx-auto mt-[2%] lg:text-[2rem] text-[1.5rem] w-[80%]">
+        <div className="flex mx-auto mt-[2%] lg:text-[2rem]  text-[1.5rem] lg:w-[80%] w-[90%] lg:pt-0 pt-5">
           <h1>1 USDT = {conversionRate} THB</h1>
         </div>
-        <div className="flex  mx-auto mt-[2%] text-[2rem] w-[80%]">
+        <div className="flex  mx-auto mt-[2%] lg:text-[2rem] lg:pt-0 pt-5 lg:w-[80%] w-[90%] text-[1.5rem]">
           <h1>Calculated Amount : {convertedAmount} THB</h1>
         </div>
         {/* btn div  */}
-        <div className="flex mx-auto mt-[2%] w-[80%] justify-between">
+        <div className="flex mx-auto mt-[2%] lg:w-[80%] w-[90%] lg:pt-0 pt-5 justify-between">
           {/* Convert btn  */}
 
           <button className="convert-btn"
