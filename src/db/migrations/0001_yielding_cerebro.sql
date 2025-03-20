18a69d69e5768041f84cad54050ea300278d7b78
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-20 23:37:34 +0530

    Schema updated

---
+++ src/db/migrations/0001_yielding_cerebro.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users" ALTER COLUMN "name" SET DATA TYPE varchar(50);--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "email" SET DATA TYPE varchar(100);--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "password" SET DATA TYPE varchar(50);
